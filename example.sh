bf77101752c7de18d59bea94f83ccae36d43967d
Andrew Darqui <andrew.darqui@gmail.com>
2013-12-24 16:07:52 -0500

    more simple example

--- example.sh
+++ example.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-mkdir -p /tmp/company1/{movies,movies2,dest,bin}
-mkdir -p /tmp/company2/{misc,out,bin,new_releases}
+mkdir -p /tmp/{incoming,dest}
 
-cp scripts/relocate.sh /tmp/company1/bin
-cp scripts/relocate.sh /tmp/company2/bin
+cp scripts/relocate.sh /tmp/
 
-chmod 755 /tmp/company*/bin/*
+chmod 755 /tmp/relocate.sh
 
-for i in `seq 1 50`; do touch /tmp/company1/movies/$i.mov; done
-for i in `seq 1 20`; do touch /tmp/company1/movies2/$i.wmv; done
-for i in `seq 1 100`; do touch /tmp/company2/new_releases/$i.jpg; done
+for i in `seq 1 50`; do touch /tmp/incoming/$i.mov; done
+for i in `seq 1 20`; do touch /tmp/incoming/$i.wmv; done
+for i in `seq 1 100`; do touch /tmp/incoming/$i.jpg; done
